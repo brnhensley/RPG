dbaba296af29f197f586834feaba23693634776f
brian hensley and mathew akre <brian+mathew@>
2019-05-30 09:07:57 -0700

    pass create pauly test

--- spec/character-spec.js
+++ spec/character-spec.js
@@ -8,3 +8,22 @@ describe('Character', function() {
     expect(defaultCharacter.health).toEqual(100);
   });
 });
+
+describe('Character', function() {
+  it('Should create Pauly', function() {
+    let pauly = new Character({
+      name: "Pauly",
+      occupation: "Poopsmith",
+      strength: 5,
+      magic: 5,
+      intelligence: 5,
+      inventory: ['poop ingot', 'poop hammer', 'porcelain anvil' ]
+    });
+    expect(pauly.name).toEqual('Pauly');
+    expect(pauly.occupation).toEqual('Poopsmith');
+    expect(pauly.strength).toEqual(5);
+    expect(pauly.magic).toEqual(5);
+    expect(pauly.inventory).toEqual(['poop ingot', 'poop hammer']);
+    expect(pauly.intelligence).toEqual(5);
+  });
+});
